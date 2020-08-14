71ef8991ee49f8ba7466965d935ed79f5ef60db2
csckiemthanh <dungnt620@wru.vn>
2020-08-14 10:25:18 +0700

    list cons ui

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
-
 const init = () => {
-    console.log('windown loaded')
-    view.setActiveScreen('registerScreen');
-    document.getElementById('redirect-to-login').addEventListener('click',(event)=>{
-        view.setActiveScreen('loginScreen')
-        document.getElementById('redirect-to-register').addEventListener('click',(event)=>{
-            view.setActiveScreen('registerScreen')
-        })
-    })
-    
-}
-window.onload=init
+    // Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyA5y7z5vHLPUaSQxQjJp8agm93j-dgHNM0",
+      authDomain: "chat-app-aed27.firebaseapp.com",
+      databaseURL: "https://chat-app-aed27.firebaseio.com",
+      projectId: "chat-app-aed27",
+      storageBucket: "chat-app-aed27.appspot.com",
+      messagingSenderId: "318133683614",
+      appId: "1:318133683614:web:f39b23275de951caf4428f"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    console.log(firebase.app().name)
+    view.setActiveScreen('registerScreen')
+  }
+  window.onload = init
--- js/view.js
+++ js/view.js
@@ -1,48 +1,48 @@
 const view = {}
 view.setActiveScreen = (screenName) => {
-switch (screenName) {
-case 'welcomeScreen' :
-    document.getElementById('app')
-    .innerHTML = components.welcomeScreen
+  switch (screenName) {
+    case 'welcomeScreen' :
+      document.getElementById('app')
+      .innerHTML = components.welcomeScreen
     break;
-case 'registerScreen':
-    document.getElementById('app')
-    .innerHTML = components.registerScreen
-    document.getElementById('redirect-to-login').addEventListener('click',(event)=>{
-        view.setActiveScreen('loginScreen')
-        document.getElementById('redirect-to-register').addEventListener('click',(event)=>{
-            view.setActiveScreen('registerScreen')
-        })
-    })
-    const registerForm=document.getElementById('register-form')
-    registerForm.addEventListener('submit',(event)=> {
-        event.preventDefault()
-        
-    const data={
-        firstName: registerForm.firstName.value,
-        lastName : registerForm.lastName.value,
-        email    : registerForm.email.value,
-        password : registerForm.password.value,
-        comfirmPassword : registerForm.comfirmPassword.value
-    }
-    console.log(data)
-    controller.register(data)
-    
-    }) 
+    case 'loginScreen' :
+    // in ra man login
+      document.getElementById('app').innerHTML = components.loginScreen
+      document.getElementById('redirect-to-register')
+      .addEventListener('click', () => {
+        view.setActiveScreen('registerScreen')
+      })
+      const loginForm = document.getElementById('login-form')
+      loginForm.addEventListener('submit', (e) => {
+        e.preventDefault()
+        loginForm.email.value = loginForm.email.value.trim()
+        const data = {
+          email: loginForm.email.value,
+          password: loginForm.password.value
+        }
+        controller.login(data)
+      })
     break;
-case 'loginScreen':
-    document.getElementById('app')
-    .innerHTML = components.loginScreen
-    const loginForm = document.getElementById('login-form')
-    loginForm.addEventListener('submit',(event)=>{
+    case 'registerScreen' :
+      document.getElementById('app')
+      .innerHTML = components.registerScreen
+      const registerForm = document.getElementById('register-form')
+      registerForm.addEventListener('submit', (event) => {
         event.preventDefault()
-        const data ={
-            email: loginForm.email.value,
-            password : loginForm.password.value,
+        const data = {
+          firstName: registerForm.firstName.value,
+          lastName: registerForm.lastName.value,
+          email: registerForm.email.value,
+          password: registerForm.password.value,
+          confirmPassword: registerForm.confirmPassword.value,
         }
         console.log(data)
-        controller.login(data)
-    })
-    
-}
+        controller.register(data)
+      })
+      document.getElementById('redirect-to-login')
+      .addEventListener('click', () => {
+        view.setActiveScreen('loginScreen')
+      })
+    break;
+  }
 }
